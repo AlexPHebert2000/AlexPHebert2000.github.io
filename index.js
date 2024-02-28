a00d69e47fb65c8cdc4ba94c48795bef3796b8d8
Alex Hebert <aphebert9994@gmail.com>
2024-02-28 15:19:44 -0600

    Added capability to filter tweets to a single user

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const DEBUG = true;
+
 /* eslint-disable no-use-before-define */
 /* eslint-disable func-style */
 
@@ -5,7 +7,8 @@ $(document).ready(() => {
   /*FUNCTIONS*/
   function createTweet(tweet) {
     const $tweet = $('<div class=tweet></div>');
-    const $user = $(`<div class=user-name>@${tweet.user}:</div>`);
+    const $user = $(`<div class=user-name data-name=${tweet.user}>@${tweet.user}:</div>`);
+    $('.user-name').on('click', userNameClickEventHelper );
     const $message = $(`<div class=message>${tweet.message}</div>`);
     const $timestamp = $(`<div class=timestamp data-timestamp=${tweet.created_at.toISOString()}>Posted: ${moment(tweet.created_at).fromNow()}</div>`);
     $tweet.append($user, $message, $timestamp);
@@ -18,12 +21,28 @@ $(document).ready(() => {
   }
 
   function updateFeed() {
-    const newTweets = streams.home.slice(currentTweetCount + 1);
+    const newTweets = streams.home.slice(currentTweetCount + 1).filter(tweet => filter ? tweet.user === filter : true);
     currentTweetCount = streams.home.length;
     addTweets(newTweets);
     $('.timestamp').each(function(index, element) { $(element).text(`Posted: ${moment($(element).attr('data-timestamp')).fromNow()}`); });
+
+    if (DEBUG) { debug(); }
+
     setTimeout(updateFeed, 500);
   }
+
+  function userNameClickEventHelper(event) {
+    filter = $(this).attr('data-name');
+    $feedDiv.html('');
+    filteredTweets = streams.home.filter(tweet => tweet.user === filter);
+    addTweets(filteredTweets);
+  }
+
+  function debug() {
+    console.log(filter);
+  }
+
+  let filter = void 0;
 
   //initialize current tweet count
   let currentTweetCount = 0;
