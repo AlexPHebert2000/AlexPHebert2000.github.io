ebc4f01942a324977701569a7ef00332a7be52c4
Alex Hebert <aphebert9994@gmail.com>
2024-02-28 11:13:48 -0600

    Added feed div and padding between tweets.

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ $(document).ready(() => {
   const $body = $('body');
   $body.html('');
 
+  $feedDiv = $('<div id=feed></div>');
+  $body.append($feedDiv);
+
   const $tweets = streams.home.map((tweet) => {
-    const $tweet = $('<div></div>');
+    const $tweet = $('<div class=tweet></div>');
     const text = `@${tweet.user}: ${tweet.message}`;
 
     $tweet.text(text);
 
     return $tweet;
   });
-  $body.append($tweets);
+  $feedDiv.append($tweets);
 
+  $('div.tweet').css('padding', '10px 20px')
 });
