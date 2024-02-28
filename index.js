bf4c0ca3f19de2ff0031c714e4d0359b8f485ac2
Alex Hebert <aphebert9994@gmail.com>
2024-02-28 11:24:02 -0600

    Created createTweet function and refactored inital tweet adding

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-
+/* eslint-disable func-style */
 $(document).ready(() => {
+
+  function createTweet(tweet) {
+    const $tweet = $('<div class=tweet></div>');
+    const text = `@${tweet.user}: ${tweet.message}`;
+    $tweet.text(text);
+    return $tweet;
+  }
+
+  let currentTweetCount = 10;
+
+
   const $body = $('body');
   $body.html('');
 
   $feedDiv = $('<div id=feed></div>');
   $body.append($feedDiv);
 
-  const $tweets = streams.home.map((tweet) => {
-    const $tweet = $('<div class=tweet></div>');
-    const text = `@${tweet.user}: ${tweet.message}`;
-
-    $tweet.text(text);
-
-    return $tweet;
-  });
+  const $tweets = streams.home.map(createTweet);
   $feedDiv.append($tweets);
 
-  $('div.tweet').css('padding', '10px 20px')
+  $('div.tweet').css('padding', '10px 20px');
 });
