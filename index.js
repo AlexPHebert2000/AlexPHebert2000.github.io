5a2170abceb14116502d063293d122fc75474f6b
Alex Hebert <aphebert9994@gmail.com>
2024-02-28 11:42:42 -0600

    Added updateFeed function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable func-style */
 $(document).ready(() => {
-
+  /*FUNCTIONS*/
   function createTweet(tweet) {
     const $tweet = $('<div class=tweet></div>');
     const text = `@${tweet.user}: ${tweet.message}`;
@@ -10,20 +11,29 @@ $(document).ready(() => {
 
   function addTweets(tweets) {
     const $tweets = tweets.map(createTweet);
-    $feedDiv.append($tweets);
+    $feedDiv.prepend($tweets);
   }
 
+  function updateFeed() {
+    const newTweets = streams.home.slice(currentTweetCount + 1);
+    currentTweetCount = streams.home.length;
+    addTweets(newTweets.map(createTweet));
+    setTimeout(updateFeed, 500);
+  }
+  /*VARIABLES*/
+  //initialize current tweet count
   let currentTweetCount = 10;
-
   //save body tag
   const $body = $('body');
+
+  /*BODY*/
   //reset body
   $body.html('');
 
   //create feed div
   $feedDiv = $('<div id=feed></div>');
   //add feed div to body
-  $body.append($feedDiv);
+  $body.prepend($feedDiv);
 
   //add initial 10 tweets to body
   addTweets(streams.home);
