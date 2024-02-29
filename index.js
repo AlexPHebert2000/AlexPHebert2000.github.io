6f74658aaaee233231ad810465c359ffe9a53242
Alex Hebert <aphebert9994@gmail.com>
2024-02-29 09:54:11 -0600

    Changed styling

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ $(document).ready(() => {
   /*FUNCTIONS*/
   function createTweet(tweet) {
     const $tweet = $('<div class=tweet></div>');
-    const $user = $(`<div class=user-name data-name=${tweet.user}>@${tweet.user}:</div>`);
+    const $user = $(`<div class='user-name tweet-body' data-name=${tweet.user}>@${tweet.user}:</div>`);
     $user.on('click', userNameClickEventHelper );
-    const $message = $(`<div class=message>${tweet.message}</div>`);
-    const $timestamp = $(`<div class=timestamp data-timestamp=${tweet.created_at.toISOString()}>Posted: ${moment(tweet.created_at).fromNow()}</div>`);
+    const $message = $(`<div class='message tweet-body'>${tweet.message}</div>`);
+    const $timestamp = $(`<div class='timestamp tweet-body' data-timestamp=${tweet.created_at.toISOString()}>Posted: ${moment(tweet.created_at).fromNow()}</div>`);
     $tweet.append($user, $message, $timestamp);
     return $tweet;
   }
@@ -24,7 +24,7 @@ $(document).ready(() => {
     const newTweets = streams.home.slice(currentTweetCount).filter(tweet => filter ? tweet.user === filter : true);
     currentTweetCount = streams.home.length;
     addTweets(newTweets);
-    $('.timestamp').each(function(index, element) { $(element).text(`Posted: ${moment($(element).attr('data-timestamp')).fromNow()}`); });
+    $('.timestamp').each(function(index, element) { $(element).text(`${moment($(element).attr('data-timestamp')).fromNow()}`); });
 
     if (DEBUG) { debug(); }
 
@@ -75,13 +75,6 @@ $(document).ready(() => {
   //add feed div to body
   $body.prepend($feedDiv);
 
-  //create tweet input form
-  $userTweetInputDiv = $('<div id=user-tweet-input-div></div>');
-  $userTweetForm = $('<form><label for=username>Username</label><input value=user type=text id=input-username></input><input value="What\'s on your mind" type=text id=input-tweet></input><input id=submit-button type=submit value="Let us know!"></form>');
-  $userTweetForm.on('submit', submitUserTweet);
-  $userTweetInputDiv.append($userTweetForm);
-  $body.prepend($userTweetInputDiv);
-
   //create filter clear button
   $filterClearDiv = $('<div id=filter-clear-div></div>');
   $filterClearButton = $('<button id=filter-clear-button>Clear Filter</button>');
@@ -90,6 +83,12 @@ $(document).ready(() => {
   $body.prepend($filterClearDiv);
   $filterClearDiv.hide();
 
+  //create tweet input form
+  $userTweetInputDiv = $('<div id=user-tweet-input-div></div>');
+  $userTweetForm = $('<form><label for=username>Username</label><input value=user type=text id=input-username></input><input value="What\'s on your mind" type=text id=input-tweet></input><input id=submit-button type=submit value="Let us know!"></form>');
+  $userTweetForm.on('submit', submitUserTweet);
+  $userTweetInputDiv.append($userTweetForm);
+  $body.prepend($userTweetInputDiv);
 
   //start update feed
   updateFeed();
